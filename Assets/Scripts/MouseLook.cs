5170bab5431772237d6f1343d6dc0ee00b036c0c
SeanCampbell255 <campbellsean211@gmail.com>
2022-03-17 09:44:21 -0400

    added comments
    went harder than usual on it :/

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -5,26 +5,30 @@ using UnityEngine;
 public class MouseLook : MonoBehaviour
 {
     
-    public float mouseSensitivity = 100f;
+    public float mouseSensitivity = 100f; //adjustable sensitivity
 
-    public Transform playerBody;
+    public Transform playerBody; //player's model/transform
 
-    float xRotation = 0f;
+    float xRotation = 0f; //updated by mouse movement, updates player rotation
 
     void Start()
     {
+        //keeps cursor in game window
         Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
+        //set axis rotation direction & magnitude relative to frame time
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
+        //set and clamp xRotation based on mouse y-axis movement;
         xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f); //clamp so can't speeeeen
 
+        //updates the player with rotations
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
     }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,43 +4,47 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    
-    public CharacterController controller;
+    public float speed = 12f; //player move speed
+    public float gravity = -9.81f; //player only gravity
+    public float groundDistance = 0.4f; //allowable margin for ground check
+    public float jumpHeight = 3f; //not force; used as height in calc
 
-    public float speed = 12f;
-    public float gravity = -9.81f;
-    public float groundDistance = 0.4f;
-    public float jumpHeight = 3f;
+    public Transform groundCheck; //transform on bottom of player for ground check
 
-    public Transform groundCheck;
+    public LayerMask groundMask; //layer that contains 'ground'; used for jump reset
 
-    public LayerMask groundMask;
+    public CharacterController controller; //convenient non-physics controller for movement, collisions, etc
 
-    Vector3 velocity;
-    bool isGrounded;
+    Vector3 velocity; //vector-form velocity used to move player when jumping
+    bool isGrounded; //tells if player is on ground for jump reset
     
     // Update is called once per frame
     void Update()
     {
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask); //check from bottom player if ground is in acceptable range to consider grounded
 
+        //if it's grounded want it to stick to ground instead of float 'groundDistance' above it
         if(isGrounded && velocity.y < 0){
             velocity.y = -2f;
         }
 
+        //getting input
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
+        //'move' is vector representing 
         Vector3 move = transform.right * x + transform.forward * z;
 
+        //move player toward input direction with 'speed' magnitude
         controller.Move(move * speed * Time.deltaTime);
 
+        //if input jump and isGrounded, move player vertically resisting gravity
         if(Input.GetButtonDown("Jump") && isGrounded){
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
 
+        //updates vertical movement based on gravity
         velocity.y += gravity * Time.deltaTime;
-
         controller.Move(velocity * Time.deltaTime);
     }
 }
